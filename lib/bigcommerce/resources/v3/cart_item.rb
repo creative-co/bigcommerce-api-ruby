1202fc1b1a4664475fcf192afb4b63b1d19414f3
alexeyanosov <weddin@ya.ru>
2021-01-26 17:44:03 +0400

    fix formatting response when create cart_item in V3

--- lib/bigcommerce/resources/v3/cart_item.rb
+++ lib/bigcommerce/resources/v3/cart_item.rb
@@ -4,6 +4,10 @@ module Bigcommerce
       include Bigcommerce::SubresourceActions.new uri: 'carts/%s/items/%s'
 
       property :id
+
+      def self.create(cartId, params)
+        Bigcommerce::V3::Cart.post path.build(cartId), params
+      end
     end
   end
 end
